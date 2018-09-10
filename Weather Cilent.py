8a69cfa46f69b7528ddbad5675284a7a0fd62ca6
Morgan Shan <mshan0290@gmail.com>
2018-09-10 10:33:21 -0700

    more search options

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -6,13 +6,21 @@
 
 def main():
 
-    code = input("What zip code do you want to search for: ")
-    print(code)
-    get_html(code)
+    search_type = input("What do you want to use to search for weather('zip' or 'city'): ")
+    print(search_type)
+    search_type = search_type.lower
+    get_html(search_type)
 
 
-def get_html(zip_code):
-    url = "api.openweathermap.org/data/2.5/weather?zip={}".format(zip_code)
+def get_html(search_type):
+    if search_type == "city":
+        city = input("Enter the city you want weather for: ")
+        url = "api.openweathermap.org/data/2.5/forecast/daily?q={}".format(city)
+        print(url)
+    elif search_type == "zip":
+        zip_code = input("Enter zip code you want weather for: ")
+        url = "api.openweathermap.org/data/2.5/forecast/daily?zip={}".format(zip_code)
+        print(url)
 
 
 if __name__ == "__main__":
