52c1b3de9bb0e4647cac039e30aaca81960a45c4
Morgan Shan <mshan0290@gmail.com>
2018-09-12 12:18:51 -0700

    added print statements to response text

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -14,18 +14,22 @@ def main():
 def get_html(search_type, api_key):
     if search_type.lower() == "city":
         city = input("Enter the city you want weather for: ")
-        url = "http://api.openweathermap.org/data/2.5/forecast?q={}&mode=xml&appid={}".format(city, api_key)
-        # print(url)
+        url = "http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}".format(city, api_key)
+        print(url)
         response = requests.get(url)
         # print(response.status_code)
-        print(response.json)
+        # pprint.pprint(response.text)
+        print(response.text)
+        print(response.text[1][1][0])
     elif search_type.lower() == "zip":
         zip_code = input("Enter zip code you want weather for: ")
         url = "http://api.openweathermap.org/data/2.5/forecast?zip={}&appid={}".format(zip_code, api_key)
-        # print(url)
+        print(url)
         response = requests.get(url)
         # print(response.status_code)
-        print(response.json)
+        # pprint.pprint(response.text)
+        print(response.text)
+        print(response.text[1][1][0])
 
 
 if __name__ == "__main__":
