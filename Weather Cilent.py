e22d1347bb20da5d7eb90ace619599a6f2968692
Morgan Shan <mshan0290@gmail.com>
2018-09-13 10:38:59 -0700

    changed way getting data works

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -1,35 +1,45 @@
-import requests
+import urllib.request
 import pprint
 import json
-import time
+# import time
+
+api_key = "b42093d17fb4129561f860377096a44a"
 
 
 def main():
 
     search_type = input("What do you want to use to search for weather('zip' or 'city'): ")
-    api_key = "b42093d17fb4129561f860377096a44a"
-    get_html(search_type, api_key)
+    url = get_html(search_type, api_key)
+    data = get_data(url)
+    pprint.pprint(data)
+    # for list_index in range(0, 40):
+        pprint.pprint(data["list"][40][3])
 
 
+def get_data(url):
+    response = urllib.request.Request(url)
+    json_object = urllib.request.urlopen(response)
+    json_data = json.load(json_object)
+    # pprint.pprint(json_data)
+    return json_data
+
+
+# receives a search type and returns the respective url
 def get_html(search_type, api_key):
     if search_type.lower() == "city":
         city = input("Enter the city you want weather for: ")
         url = "http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}".format(city, api_key)
         print(url)
-        response = requests.get(url)
-        # print(response.status_code)
-        # pprint.pprint(response.text)
-        print(response.text)
-        print(response.text[1][1][0])
+        return url
     elif search_type.lower() == "zip":
-        zip_code = input("Enter zip code you want weather for: ")
+        zip_code = int(input("Enter zip code you want weather for: "))
         url = "http://api.openweathermap.org/data/2.5/forecast?zip={}&appid={}".format(zip_code, api_key)
         print(url)
-        response = requests.get(url)
-        # print(response.status_code)
-        # pprint.pprint(response.text)
-        print(response.text)
-        print(response.text[1][1][0])
+        return url
+    else:
+        print("----- Invalid response -----""\n")
+        search_type = input("What do you want to use to search for weather('zip' or 'city'): ")
+        return get_html(search_type, api_key)
 
 
 if __name__ == "__main__":
