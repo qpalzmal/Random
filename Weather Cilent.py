838b6d66196d1e80327d7bcfc4686a53ab942c6e
Morgan Shan <mshan0290@gmail.com>
2018-09-14 12:15:18 -0700

    fixed unit input line

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -1,57 +1,43 @@
 import urllib.request as req
 import pprint
 import json
-# import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 api_key = "b42093d17fb4129561f860377096a44a"
+temperature_list = []
 
 
 def main():
 
-    search_type = input('What do you want to use to search for weather("zip", "city"): ')
-    url = get_html(search_type, api_key)
-    temperature_units = input("Enter what units you want the temperature in: ")
+    # asks user for units of measurement, unit will default to fahrenheit if no valid answer given
+    units = input("Enter what units you want the temperature in: ")
+    if units.lower() == "celsius" or units.lower() == "c":
+        units = "metric"
+    elif units.lower() == "kelvin" or units.lower() == "k":
+        units = "default"
+    else:
+        units = "imperial"
+
+    search = input('What do you want to use to search for weather("zip", "city"): ')
+    url = get_html(search, units, api_key)
+
     data = get_data(url)
 
     # pprint.pprint(data)
     # method used to reach the temperature
     # pprint.pprint(data["list"][0]["main"]["temp"])
 
-    # gets a whole list of temperatures for use
-    temperature_list = []
-
     # receives all the temperatures and puts it into a list
     for index_count in range(40):
         # print(data["list"][index_count]["main"]["temp"])
-        #  gets the temperature
+        # gets the temperature
         temperature = data["list"][index_count]["main"]["temp"]
         temperature_list.append(temperature)
         index_count += 1
     pprint.pprint(temperature_list)
-    print("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
-    # if temperature_units.lower() == "fahrenheit" or temperature_units == "f":
-    #     for temperature in range(len(temperature_list)):
-    #         temp_list = []
-    #         temp.list.append[]
-    if temperature_units.lower() == "celsius" or temperature_units == "c":
-        for temperature in temperature_list:
-            temp_list = []
-            temp_list.append(temperature - 273)
-            # temperature_list.remove(temperature)
-        # temp_list = temperature_list
-    else:
-        print("----- Invalid response -----")
-        print("")
-        temperature_units = input("Enter what units you want the temperature in: ")
-# # converts the given temperature(kelvin) and converts to celsius
-
-# celsius_temperature = round(kelvin_temperature - 273, 2)
-
-    pprint.pprint(temp_list)
 
 # goes to the url and sends back the data for use
 def get_data(url):
@@ -62,22 +48,21 @@ def get_data(url):
 
 
 # receives a search type and returns the respective url
-def get_html(search_type, api_key):
-    if search_type.lower() == "city":
+def get_html(search, units, key):
+    if search.lower() == "city":
         city = input("Enter the city you want weather for: ")
-        url = "http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}".format(city, api_key)
+        url = "http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&appid={}".format(city, units, key)
         # print(url)
         return url
-    elif search_type.lower() == "zip":
+    elif search.lower() == "zip":
         zip_code = int(input("Enter zip code you want weather for: "))
-        url = "http://api.openweathermap.org/data/2.5/forecast?zip={}&appid={}".format(zip_code, api_key)
+        url = "http://api.openweathermap.org/data/2.5/forecast?zip={}&units={}&appid={}".format(zip_code, units, key)
         # print(url)
         return url
     else:
         print("----- Invalid response -----")
-        print("")
-        search_type = input('What do you want to use to search for weather("zip", "city"): ')
-        return get_html(search_type, api_key)
+        search = input('What do you want to use to search for weather("zip", "city"): ')
+        return get_html(search, units, key)
 
 
 if __name__ == "__main__":
