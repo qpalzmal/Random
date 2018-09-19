e4b7a9fce88f4be4b5caa493158f33cd716529ff
Morgan Shan <mshan0290@gmail.com>
2018-09-19 12:42:12 -0700

    added utc to local time converter (contains errors)

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -4,6 +4,9 @@ import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+from dateutil import tz
+
 
 api_key = "b42093d17fb4129561f860377096a44a"
 temperature_list = []
@@ -14,7 +17,7 @@ time_list = []
 def main():
 
     # asks user for units of measurement, unit will default to fahrenheit if no valid answer given
-    units = input("Enter what units you want the temperature in(defaults to Fahrenheit(F) if there are any typos): ")
+    units = input("Enter what units you want the temperature in (defaults to Fahrenheit(F) if there are any typos): ")
     if units.lower() == "celsius" or units.lower() == "c":
         print("Temperature units has been set to Celsius")
         units = "metric"
@@ -47,28 +50,42 @@ def main():
     # pprint.pprint(temperature_list)
     # pprint.pprint(date_list)
 
-    # gets the time from the return date + time value
+    # gets the only the "time" from the return "date + time" value
     for i in date_list:
         time_list.append(i[11:])
-
     # pprint.pprint(time_list)
-    test_list = []
-    for i in range(40):
-        temp = temperature_list[i]
-        time = time_list[i]
-        test_list.append(str(temp) + " = " + time)
-
-    pprint.pprint(test_list)
-    plt.scatter(time_list, temperature_list)
-    plt.show()
 
 
-# goes to the url and sends back the data for use
-def get_data(url):
-    response = req.Request(url)
-    json_object = req.urlopen(response)
-    json_data = json.load(json_object)
-    return json_data
+
+
+
+    # METHOD 2: Auto-detect zones:
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+
+    # utc = datetime.utcnow()
+    for i in date_list:
+       utc = datetime.strptime(i, '%Y-%m-%d %H:%M:%S')
+
+        # Tell the datetime object that it's in UTC time zone since
+        # datetime objects are 'naive' by default
+        utc = utc.replace(tzinfo=from_zone)
+
+        # Convert time zone
+        central = utc.astimezone(to_zone)
+
+    pprint.pprint(date_list)
+
+    # shows temperature that is equal to the time
+    # test_list = []
+    # for i in range(40):
+    #     temp = temperature_list[i]
+    #     time = time_list[i]
+    #     test_list.append(str(temp) + " = " + time)
+    # pprint.pprint(test_list)
+
+    # plt.scatter(time_list, temperature_list)
+    # plt.show()
 
 
 # receives a search type and returns the respective url
@@ -87,6 +104,14 @@ def get_html(search, units, key):
         print("----- Invalid response -----")
         search = input('What do you want to use to search for weather("zip", "city"): ')
         return get_html(search, units, key)
+
+
+# goes to the url and sends back the data for use
+def get_data(url):
+    response = req.Request(url)
+    json_object = req.urlopen(response)
+    json_data = json.load(json_object)
+    return json_data
 
 
 if __name__ == "__main__":
