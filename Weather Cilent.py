ae1d2309586d30f8544feeacf9f07b14228d8d5d
Morgan Shan <mshan0290@gmail.com>
2018-09-20 10:38:41 -0700

    fixed time converter and changed scatter plot to regular plot

--- Weather Cilent.py
+++ Weather Cilent.py
@@ -1,115 +1,127 @@
-import urllib.request as req
+'''
+This program scraps the web for data and uses it to graph the data for analysis by the user
+Section cut off with line of commented "-" is code from someone else modified to work here
+'''
+
+
+import urllib.request
 import pprint
 import json
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 from dateutil import tz
 
 
-api_key = "b42093d17fb4129561f860377096a44a"
-temperature_list = []
-date_list = []
-time_list = []
+API_KEY = "b42093d17fb4129561f860377096a44a"
 
 
 def main():
 
-    # asks user for units of measurement, unit will default to fahrenheit if no valid answer given
-    units = input("Enter what units you want the temperature in (defaults to Fahrenheit(F) if there are any typos): ")
+    # asks user for units of measurement, unit will default to fahrenheit if any other answer is given
+    units = input("Enter what units you want the temperature in (type either the letter symbol or the word): ")
     if units.lower() == "celsius" or units.lower() == "c":
-        print("Temperature units has been set to Celsius")
+        print("Temperature units has been set to Celsius (°C)")
         units = "metric"
     elif units.lower() == "kelvin" or units.lower() == "k":
-        print("Temperature units has been set to Kelvin")
+        print("Temperature units has been set to Kelvin (K)")
         units = "default"
     else:
-        print("Temperature units has been set to Fahrenheit")
+        print("Temperature units has been set to Fahrenheit (°F)")
         units = "imperial"
 
-    search = input("\n"'What do you want to use to search for weather("zip", "city"): ')
-    url = get_html(search, units, api_key)
+    search = input("\n"'What do you want to use to search for weather ("zip", "city"): ')
+    url = get_html(search, units, API_KEY)
 
     data = get_data(url)
-    pprint.pprint(data)
+    # pprint.pprint(data)
 
     # method used to reach the temperature and time
     # pprint.pprint(data["list"][0]["main"]["temp"])
     # pprint.pprint(data["list"][0]["dt_txt"]
 
-    for index_count in range(len(data["list"])):
+    # receives the temperature and date+time from the data
+    temperature_list = []
+    datetime_list = []
+    for index in range(len(data["list"])):
         # gets the temperature
-        temperature = data["list"][index_count]["main"]["temp"]
+        temperature = data["list"][index]["main"]["temp"]
         temperature_list.append(temperature)
         # gets the time
-        date = data["list"][index_count]["dt_txt"]
-        date_list.append(date)
-        index_count += 1
+        date_time = data["list"][index]["dt_txt"]
+        datetime_list.append(date_time)
+        index += 1
 
     # pprint.pprint(temperature_list)
-    # pprint.pprint(date_list)
+    # pprint.pprint(datetime_list)
 
+    # (----NOT USED----)  DELETE LATER
     # gets the only the "time" from the return "date + time" value
-    for i in date_list:
-        time_list.append(i[11:])
+    # time_list = []
+    # for i in datetime_list:
+    #     time_list.append(i[11:])
     # pprint.pprint(time_list)
 
-
-
-
-
+    # ------------------------------------------------------------------------------------------------------------------
+    local_time_list = []
     # METHOD 2: Auto-detect zones:
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
 
     # utc = datetime.utcnow()
-    for i in date_list:
-       utc = datetime.strptime(i, '%Y-%m-%d %H:%M:%S')
+    for index in datetime_list:
+        # print(index)
+        utc = datetime.strptime(index, '%Y-%m-%d %H:%M:%S')
+        # print(utc)
 
         # Tell the datetime object that it's in UTC time zone since
         # datetime objects are 'naive' by default
         utc = utc.replace(tzinfo=from_zone)
+        # print(utc)
 
         # Convert time zone
-        central = utc.astimezone(to_zone)
+        local = utc.astimezone(to_zone)
+        # print(local)
+        local_time_list.append(local)
 
-    pprint.pprint(date_list)
+    # pprint.pprint(local_time_list)
+    # ------------------------------------------------------------------------------------------------------------------
 
+    # (----NOT USED----)  DELETE LATER
     # shows temperature that is equal to the time
-    # test_list = []
+    # temperature_time_list = []
     # for i in range(40):
     #     temp = temperature_list[i]
     #     time = time_list[i]
-    #     test_list.append(str(temp) + " = " + time)
-    # pprint.pprint(test_list)
+    #     temperature_time_list.append(str(temp) + " = " + time)
+    # pprint.pprint(temperature_time_list)
 
-    # plt.scatter(time_list, temperature_list)
-    # plt.show()
+    # plt.scatter(local_time_list, temperature_list)
+    plt.plot(local_time_list, temperature_list)
+    plt.show()
 
 
 # receives a search type and returns the respective url
 def get_html(search, units, key):
     if search.lower() == "city":
-        city = input("Enter the city you want weather for: ")
+        city = input("Enter the city you want the weather for: ")
         url = "http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&appid={}".format(city, units, key)
         # print(url)
         return url
     elif search.lower() == "zip":
-        zip_code = int(input("Enter zip code you want weather for: "))
+        zip_code = int(input("Enter zip code you want the weather for: "))
         url = "http://api.openweathermap.org/data/2.5/forecast?zip={}&units={}&appid={}".format(zip_code, units, key)
         # print(url)
         return url
     else:
         print("----- Invalid response -----")
-        search = input('What do you want to use to search for weather("zip", "city"): ')
+        search = input('What do you want to use to search for the weather ("zip", "city"): ')
         return get_html(search, units, key)
 
 
 # goes to the url and sends back the data for use
 def get_data(url):
-    response = req.Request(url)
-    json_object = req.urlopen(response)
+    response = urllib.request.Request(url)
+    json_object = urllib.request.urlopen(response)
     json_data = json.load(json_object)
     return json_data
 
