a0238c0058a48042c7ff8c7c81b05733ee3cac12
Morgan Shan <mshan0290@gmail.com>
2018-10-16 12:16:36 -0700

    more building

--- StarCraft 2 AI.py
+++ StarCraft 2 AI.py
@@ -1,6 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race
 from sc2.player import Bot, Human
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 map_dir = "C:\\Users\CODE\Desktop\Ladder2018Season2\\16-BitLE"
 
@@ -9,17 +10,41 @@ class WorkerRushBot(sc2.BotAI):
     # on_step function is called for every game step
     # it takes current game state and iteration
     async def on_step(self, iteration):
+
+        # ---- WORKER RUSH ----
         # if it is the first frame on the game
         # if iteration == 0:
         #     for worker in self.workers:
         #         # have the worker attack the enemy start position
         #         await self.do(worker.attack(self.enemy_start_locations[0]))
+        # ---- WORKER RUSH ----
 
-        if self.supply_left <= 5:
-            self.select_build_worker(self.townhalls.center, force=True)
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_supply()
+        await self.build_assimilator()
 
+    async def build_workers(self):
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
+
+    async def build_supply(self):
+        if self.supply_left <= 6 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exist:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses(first))
+
+    async def build_assimilator(self):
+        for nexus in self.units(NEXUS).ready:
+            self.vespene_geysers = self.state.vespene_geyser.closer_than(25.0, nexus)
+            for vespene_geyser in self.vespene_geysers:
+                if self.can_afford(ASSIMILATOR):
+                    self.units.closer_than(1.0, vespene_geyser).exits:
+                        await self.build(ASSIMILATOR)
 
 run_game(maps.get(map_dir), [
     Human(Race.Protoss),
-    Bot(Race.Random, WorkerRushBot())
+    Bot(Race.PROTOSS, WorkerRushBot())
 ], realtime=True)
